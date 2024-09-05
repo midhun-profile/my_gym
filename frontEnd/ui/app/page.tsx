6ea5ba7ecabca43107a4bc04f596a67c22ee2f33
midhun <106465691+midhun-profile@users.noreply.github.com>
2024-09-05 20:41:53 +0000

    ddd

--- frontEnd/ui/app/page.tsx
+++ frontEnd/ui/app/page.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Portfolio from "../components/home/portfolio";
 import Service from "../components/home/featuresAndServices";
 import Footer from "../components/home/footer";
@@ -8,7 +7,7 @@ import Footer from "../components/home/footer";
 const Hero = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="relative bg-white pb-[110px] pt-[120px] dark:bg-dark lg:pt-[150px]">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
@@ -119,70 +118,70 @@ const SingleImage = ({ href, imgSrc }) => (
   </a>
 );
 
-const Navbar = () => {
-  const [open, setOpen] = useState(false);
+// const Navbar = () => {
+//   const [open, setOpen] = useState(false);
 
-  return (
-    <header className={`absolute left-0 top-0 z-20 flex w-full items-center`}>
-      <div className="container">
-        <div className="relative -mx-4 flex items-center justify-between">
-          <div className="w-60 max-w-full px-4">
-            <a href="/#" className="block w-full py-5">
-              <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo.svg"
-                alt="logo"
-                className="w-full dark:hidden"
-              />
-              <img
-                src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-white.svg"
-                alt="logo"
-                className="w-full hidden dark:block"
-              />
-            </a>
-          </div>
-          <div className="flex w-full items-center justify-between px-4">
-            <div>
-              <button
-                onClick={() => setOpen(!open)}
-                id="navbarToggler"
-                className={` ${open ? "navbarTogglerActive" : ""} absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
-              >
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-                <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
-              </button>
-              <nav
-                id="navbarCollapse"
-                className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:shadow-none lg:dark:bg-transparent ${!open ? "hidden" : ""}`}
-              >
-                <ul className="block lg:flex">
-                  <ListItem NavLink="/#">Home</ListItem>
-                  <ListItem NavLink="/#">Payment</ListItem>
-                  <ListItem NavLink="/#">About</ListItem>
-                  <ListItem NavLink="/#">Blog</ListItem>
-                </ul>
-              </nav>
-            </div>
-            <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
-              <a
-                href="/#"
-                className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
-              >
-                Sign in
-              </a>
-              <a
-                href="/#"
-                className="rounded-lg bg-primary px-7 py-3 text-base font-medium text-white hover:bg-opacity-90"
-              >
-                Sign Up
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
+//   return (
+//     <header className={`absolute left-0 top-0 z-20 flex w-full items-center`}>
+//       <div className="container">
+//         <div className="relative -mx-4 flex items-center justify-between">
+//           <div className="w-60 max-w-full px-4">
+//             <a href="/#" className="block w-full py-5">
+//               <img
+//                 src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo.svg"
+//                 alt="logo"
+//                 className="w-full dark:hidden"
+//               />
+//               <img
+//                 src="https://cdn.tailgrids.com/2.0/image/assets/images/logo/logo-white.svg"
+//                 alt="logo"
+//                 className="w-full hidden dark:block"
+//               />
+//             </a>
+//           </div>
+//           <div className="flex w-full items-center justify-between px-4">
+//             <div>
+//               <button
+//                 onClick={() => setOpen(!open)}
+//                 id="navbarToggler"
+//                 className={` ${open ? "navbarTogglerActive" : ""} absolute right-4 top-1/2 block -translate-y-1/2 rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden`}
+//               >
+//                 <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+//                 <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+//                 <span className="relative my-[6px] block h-[2px] w-[30px] bg-body-color dark:bg-white"></span>
+//               </button>
+//               <nav
+//                 id="navbarCollapse"
+//                 className={`absolute right-4 top-full w-full max-w-[250px] rounded-lg bg-white px-6 py-5 shadow dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:shadow-none lg:dark:bg-transparent ${!open ? "hidden" : ""}`}
+//               >
+//                 <ul className="block lg:flex">
+//                   <ListItem NavLink="/#">Home</ListItem>
+//                   <ListItem NavLink="/#">Payment</ListItem>
+//                   <ListItem NavLink="/#">About</ListItem>
+//                   <ListItem NavLink="/#">Blog</ListItem>
+//                 </ul>
+//               </nav>
+//             </div>
+//             <div className="hidden justify-end pr-16 sm:flex lg:pr-0">
+//               <a
+//                 href="/#"
+//                 className="px-7 py-3 text-base font-medium text-dark hover:text-primary dark:text-white"
+//               >
+//                 Sign in
+//               </a>
+//               <a
+//                 href="/#"
+//                 className="rounded-lg bg-primary px-7 py-3 text-base font-medium text-white hover:bg-opacity-90"
+//               >
+//                 Sign Up
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
 
 const ListItem = ({ children, NavLink }) => (
   <li>
